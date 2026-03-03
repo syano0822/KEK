737cfdb3067f83357cef3a38c3624840a0535e85
Satoshi Yano <satoshi.yano@cern.ch>
2026-03-03 13:42:40 +0900

    run_analysis.sh

--- run_analysis.sh
+++ run_analysis.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-RUNNUMBER=$1
-root 'make_event_waveforms.C("${RUNNUMBER}.root","waveform_${RUNNUMBER}.root","events",-1,-10.,-50.,20)'
-root 'make_hit_position.C("waveform_$RUNNUMBER.root","pos_${RUNNUMBER}.root",8.,14.5,0.5,900,13.,19.,0.,300.,true)'
-root 'draw_hit_position.C("$RUNNUMBER","thr_lin")'
+RUN=$1
+EXP=2026-03_RARiS
+RAW_DIR=~/data/${EXP}/physics
+ROOT_DIR=~/data/${EXP}_root/physics
+
+python /opt/eudaq/user/epic/scripts/LeCroyDump.py ${RAW_DIR}/run${RUN}.raw ${ROOT_DIR}/run${RUN}.root --output-type root 
+root make_event_waveforms.C\(\"${ROOT_DIR}/run${RUN}.root\",\"waveform_${RUN}.root\",\"events\",-1,-10.,-50.,20\)
+#root 'make_hit_position.C("waveform_${RUN}.root","pos_${RUN}.root",8.,14.5,0.5,900,13.,19.,0.,300.,true)'
+#root 'draw_hit_position.C("pos_${RUN}.root","thr_lin")'
